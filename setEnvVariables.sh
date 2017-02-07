09a66f1ead58ec7773fdc382ae3fb57d2f555b5a
Farhad hsni <farhad.hsni@gmail.com>
2017-02-07 10:03:37 +0330

    remove unnecessary files

--- setEnvVariables.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export TS_NODE_COMPILER_OPTIONS={\"module\":\"CommonJS\"\,\"target\":\"ES5\"}
-./node_modules/.bin/mocha --compilers ts:ts-node/register
--- test/test/add.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* tslint:disable only-arrow-functions */
-var chai_1 = require("chai");
-var fooModule_1 = require("../src/scripts/fooModule");
-describe('add', function () {
-    it('should add two numbers', function () {
-        chai_1.assert.deepEqual(fooModule_1.add(2, 5), 7, '2+7 should be 7');
-    });
-});
