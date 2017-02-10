7b95ff86d4b8756f14996789941a8441d3e11f52
Farhad hsni <farhad.hsni@gmail.com>
2017-02-10 19:43:25 +0330

    Use Procfile in smoke testing

--- test/server.smoke.test.ts
+++ test/server.smoke.test.ts
@@ -1,18 +1,40 @@
 // launch the server in the same way it happens in production
 // get a page
 // confirm we got something
-import { assert } from 'chai';
-import * as process from 'child_process';
-import * as http from 'http';
-import { httpGet } from './server.test';
-const command = {
-    args: ['./src/server/weewikipaint.js', '8081'],
-    command: 'node',
-};
 
-function runServer(task: { command: string, args: string[] }): Promise<process.ChildProcess> {
+import { assert } from 'chai';
+import * as ps from 'child_process';
+import * as http from 'http';
+import * as fs from 'fs';
+import { httpGet } from './server.test';
+declare var require: (moduleId: string) => any
+const procfile = require('procfile');
+let PORT = '';
+
+function readProcfile() {
+    let i: number;
+    const file = fs.readFileSync('Procfile').toString();
+    const web: {
+        command: string;
+        options: string[];
+    } = procfile.parse(file).web;
+
+    if (!process.env.PORT) {
+        web.options = web.options.map((element) => {
+            if (element === '$PORT') {
+                PORT = '5000'
+                return PORT;
+            }
+            return element;
+        });
+    }
+
+    return web;
+}
+
+function runServer(task: { command: string, options: string[] }): Promise<ps.ChildProcess> {
     return new Promise((resolve, reject) => {
-        const proc = process.spawn(task.command, task.args);
+        const proc = ps.spawn(task.command, task.options);
         proc.stdout.setEncoding('utf8');
         proc.stdout.on('data', (data) => {
             if (data.toString().trim() === 'Server started') {
@@ -24,15 +46,16 @@ function runServer(task: { command: string, args: string[] }): Promise<process.C
     });
 }
 
+const command = readProcfile();
 describe('smoke testing server', function () {
     it('should get homepage', function (done) {
         runServer(command)
             .then(serverProcess => {
-                httpGet('http://localhost:8081')
+                httpGet(`http://localhost:${PORT}`)
                     .then(data => {
                         serverProcess.kill();
                         assert.notEqual(data.responseData.indexOf('WeeWikiPaint home page'), -1,
-                                        'home page should contain WeeWikiPaint home page');
+                            'home page should contain WeeWikiPaint home page');
                         done();
                     }).catch(e => {
                         serverProcess.kill();
@@ -46,11 +69,11 @@ describe('smoke testing server', function () {
     it('should get 404 page', function (done) {
         runServer(command)
             .then(serverProcess => {
-                httpGet('http://localhost:8081/foobar')
+                httpGet(`http://localhost:${PORT}/foobar`)
                     .then(data => {
                         serverProcess.kill();
                         assert.notEqual(data.responseData.indexOf('WeeWikiPaint 404 page'), -1,
-                                        '404 page should contain WeeWikiPaint 404 page');
+                            '404 page should contain WeeWikiPaint 404 page');
                         done();
                     }).catch(e => {
                         serverProcess.kill();
