67c840f5f2b41e17a5a2706c1046a76ca23f314a
Farhad hsni <farhad.hsni@gmail.com>
2017-02-13 16:48:35 +0330

    Spike drawing a line with Raphael

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,1 +1,7 @@
 import './../styles/main.scss';
+import { initializeDrawingArea } from './init';
+
+const drawingArea = document.getElementById('drawingArea') as HTMLDivElement;
+const paper = initializeDrawingArea(drawingArea);
+
+paper.path('M100,10L300,20');
