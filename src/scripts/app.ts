0b739bbe5cecf4878cfaeedaeb739181a45c4a0d
Farhad hsni <farhad.hsni@gmail.com>
2017-02-14 13:59:59 +0330

    Spike drawing in response to user intraction

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -6,10 +6,42 @@ import { initializeDrawingArea, drawLine } from './drawingArea';
 const drawingArea = document.getElementById('drawingArea') as HTMLDivElement;
 const paper = initializeDrawingArea(drawingArea);
 
-drawLine({
-    endX: 300,
-    endY: 300,
-    startX: 100,
-    startY: 100,
-    paper,
+// drawLine({
+//     endX: 300,
+//     endY: 300,
+//     startX: 100,
+//     startY: 100,
+//     paper,
+// });
+
+const pos = drawingArea.getBoundingClientRect();
+let startX: number;
+let startY: number;
+let shouldWeDraw = false;
+
+function mouseDownHandler(mouseDownEvent: MouseEvent) {
+    shouldWeDraw = true;
+    mouseDownEvent.preventDefault();
+    startX = mouseDownEvent.clientX - pos.left;
+    startY = mouseDownEvent.clientY - pos.top;
+}
+
+document.addEventListener('mouseup', () => shouldWeDraw = false);
+drawingArea.addEventListener('mousedown', mouseDownHandler);
+drawingArea.addEventListener('mouseleave', () => shouldWeDraw = false);
+drawingArea.addEventListener('mousemove', mouseMoveEvent => {
+    if (shouldWeDraw) {
+        const endX = mouseMoveEvent.clientX - pos.left;
+        const endY = mouseMoveEvent.clientY - pos.top;
+
+        drawLine({
+            startX,
+            startY,
+            endX,
+            endY,
+            paper,
+        });
+        startX = endX;
+        startY = endY;
+    }
 });
--- src/scripts/drawingArea.ts
+++ src/scripts/drawingArea.ts
@@ -13,10 +13,6 @@ export function initializeDrawingArea(drawingAreaElement: HTMLDivElement): Rapha
     return paper;
 }
 
-// export function drawLine(startX: number, startY: number, endX: number, endY: number, paper: RaphaelPaper) {
-//     paper.path(`M${startX},${startY}L${endX},${endY}`);
-// }
-
 export function drawLine(coordinate: drawLineArgumentObject) {
     coordinate.paper.path(`M${coordinate.startX},${coordinate.startY}L${coordinate.endX},${coordinate.endY}`);
 }
