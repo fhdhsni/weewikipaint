d52834e2050fb45cfc2e74a28bde18bf434c3fd6
Farhad hsni <farhad.hsni@gmail.com>
2017-02-14 15:00:26 +0330

    Refactor drawingArea.test in favor of less duplication

--- test/client/drawingArea.test.ts
+++ test/client/drawingArea.test.ts
@@ -8,9 +8,13 @@ describe('Drawing area', function () {
     drawingArea.setAttribute('id', 'wwp-drawingArea');
     drawingArea.style.width = `${width}px`;
     drawingArea.style.height = `${height}px`;
+    let paper: RaphaelPaper;
 
     beforeEach(function () {
-        document.body.appendChild(drawingArea.cloneNode());
+        const clonedDrawingArea = drawingArea.cloneNode(true) as HTMLDivElement;
+
+        document.body.appendChild(clonedDrawingArea);
+        paper = initializeDrawingArea(clonedDrawingArea);
     });
 
     afterEach('cleaning the DOM after assertion', function () {
@@ -20,24 +24,19 @@ describe('Drawing area', function () {
 
     it('should be initialized with Raphael', function () {
         const fetchedDrawingArea = document.getElementById('wwp-drawingArea') as HTMLDivElement;
-
-        initializeDrawingArea(fetchedDrawingArea);
         const tagName = fetchedDrawingArea.children[0].tagName.toLowerCase();
 
         assert.equal(tagName, 'svg', '#wwp-drawingArea should have a SVG child');
     });
 
     it('should have the same dimension as the width and height of its parent', function () {
-        const fetchedDrawingArea = document.getElementById('wwp-drawingArea') as HTMLDivElement;
-        const paper = initializeDrawingArea(fetchedDrawingArea);
-
+        // tslint:disable-next-line
+        debugger;
         assert.equal(paper.width, width, 'Drawing area doesn\'t have the expected width');
         assert.equal(paper.height, height, 'Drawing area doesn\'t have the expected height');
     });
 
     it('should draw a line', function () {
-        const fetchedDrawingArea = document.getElementById('wwp-drawingArea') as HTMLDivElement;
-        const paper = initializeDrawingArea(fetchedDrawingArea);
         let elements: RaphaelElement[] = [];
         const coordinate = {
             endX: 300,
