c61094c210dc19a1207fc412a9eb75e60c548058
Farhad hsni <farhad.hsni@gmail.com>
2017-02-15 18:50:02 +0330

    userInteraction respects padding and margin

--- src/scripts/userInteraction.ts
+++ src/scripts/userInteraction.ts
@@ -1,24 +1,26 @@
 export function userInteraction(
-    paper: RaphaelPaper, drawingArea: HTMLDivElement, drawLine: (arg: DrawLineArgumentObject) => void) {
+    paper: RaphaelPaper, drawingArea: HTMLElement, drawLine: (arg: DrawLineArgumentObject) => void) {
     const pos = drawingArea.getBoundingClientRect();
     let startX: number;
     let startY: number;
     let shouldWeDraw = false;
+    const padding = parseInt(window.getComputedStyle(drawingArea).getPropertyValue('padding'), 10);
+    const border = parseInt(window.getComputedStyle(drawingArea).getPropertyValue('border-width'), 10);
 
     function mouseDownHandler(mouseDownEvent: MouseEvent) {
         shouldWeDraw = true;
         mouseDownEvent.preventDefault();
-        startX = mouseDownEvent.clientX - pos.left;
-        startY = mouseDownEvent.clientY - pos.top;
+        startX = mouseDownEvent.clientX - pos.left - padding - border;
+        startY = mouseDownEvent.clientY - pos.top - padding - border;
     }
 
     document.addEventListener('mouseup', () => shouldWeDraw = false);
-    drawingArea.addEventListener('mousedown', mouseDownHandler);
     drawingArea.addEventListener('mouseleave', () => shouldWeDraw = false);
+    drawingArea.addEventListener('mousedown', mouseDownHandler);
     drawingArea.addEventListener('mousemove', mouseMoveEvent => {
         if (shouldWeDraw) {
-            const endX = mouseMoveEvent.clientX - pos.left;
-            const endY = mouseMoveEvent.clientY - pos.top;
+            const endX = mouseMoveEvent.clientX - pos.left - padding - border;
+            const endY = mouseMoveEvent.clientY - pos.top - padding - border;
 
             drawLine({
                 startX,
