aa2822583d81b6b5561bfa9c683745e98991600b
Farhad hsni <farhad.hsni@gmail.com>
2017-02-23 11:30:34 +0330

    Add touch event listeners

--- src/scripts/userInteraction.ts
+++ src/scripts/userInteraction.ts
@@ -16,14 +16,38 @@ export function userInteraction(
     } = undefined;
 
     drawingArea.addEventListener('mouseup', () => start = undefined);
+    drawingArea.addEventListener('touchend', () => start = undefined);
+
     drawingArea.addEventListener('mouseleave', () => start = undefined);
-    drawingArea.addEventListener('mousedown', mouseDownEvent => {
-        mouseDownEvent.preventDefault(); // to prevent text selection or other wierd behaviors
-        start = relativeOffset(mouseDownEvent.clientX, mouseDownEvent.clientY);
+    drawingArea.addEventListener('touchleave', () => start = undefined);
+
+    drawingArea.addEventListener('mousedown', event => {
+        event.preventDefault(); // to prevent text selection or other wierd behaviors
+        start = relativeOffset(event.clientX, event.clientY);
     });
-    drawingArea.addEventListener('mousemove', mouseMoveEvent => {
+    drawingArea.addEventListener('touchstart', event => {
+        event.preventDefault(); // to prevent text selection or other wierd behaviors
+        start = relativeOffset(event.touches[0].clientX, event.touches[0].clientY);
+    });
+
+    drawingArea.addEventListener('mousemove', event => {
         if (start !== undefined) {
-            const end = relativeOffset(mouseMoveEvent.clientX, mouseMoveEvent.clientY);
+            const end = relativeOffset(event.clientX, event.clientY);
+
+            drawLine({
+                startX: start.x,
+                startY: start.y,
+                endX: end.x,
+                endY: end.y,
+                paper,
+            });
+            start.x = end.x;
+            start.y = end.y;
+        }
+    });
+    drawingArea.addEventListener('touchmove', event => {
+        if (start !== undefined) {
+            const end = relativeOffset(event.touches[0].clientX, event.touches[0].clientY);
 
             drawLine({
                 startX: start.x,
@@ -40,6 +64,13 @@ export function userInteraction(
     let timer: any;
     window.addEventListener('resize', () => {
         // we are doing this to only run getBoundingClientRect for once when resizing finished
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            drawingAreaPosition = drawingArea.getBoundingClientRect();
+        }, 100);
+    });
+    window.addEventListener('scroll', () => {
+        // we are doing this to only run getBoundingClientRect for once when scroll finished
         clearTimeout(timer);
         timer = setTimeout(() => {
             drawingAreaPosition = drawingArea.getBoundingClientRect();
